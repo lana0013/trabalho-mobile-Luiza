c925337a6f3d81e326a68db40693130cee60fe27
gigifggbgv <giovanna.verissimo.bezerra@escola.pr.gov.br>
2025-03-25 10:45:01 -0300

    commit 2

--- Mobile/app/(tabs)/index.tsx
+++ Mobile/app/(tabs)/index.tsx
@@ -11,12 +11,12 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/movie.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Olá!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -65,10 +65,13 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
+    position: 'absolute',
+    top: 0,
     left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
     position: 'absolute',
   },
 });
